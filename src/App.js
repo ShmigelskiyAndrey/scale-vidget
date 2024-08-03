ce2e9546c0b779e3a695fb1e8c1786aa0e2568f0
Andrey Shmigelskiy <shmigelskiyandrey@gmail.com>
2024-08-03 16:57:18 +0400

    added propper gradient to widget, fixed percentage visability

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 
 function App() {
+  const value = 99/4*3;
   return (
     <main>
-      <label>status <progress max={100} value={70} style={{"--p": "70%"}}>70%</progress></label>
+      <label>status <progress max={100} value={value} style={{"--p": `${value}%`}}>{value} %</progress></label>
     </main>
   );
 }
